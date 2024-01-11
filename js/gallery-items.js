ae32e429c44c2f599b796e6c36e88a6ad93c7a85
Tihi Florin <tihi.florin66@gmail.com>
2024-01-12 01:15:56 +0200

    1st task

---
+++ js/gallery-items.js
@@ -0,0 +1,47 @@
+export const galleryItems = [
+  {
+    preview: "/images/rchids-4202820__340.jpg",
+    original: "/images/rchids-4202820_1280.jpg",
+    description: "Hokkaido Flower",
+  },
+  {
+    preview: "/images/container-4203677__340.jpg",
+    original: "/images/container-4203677_1280.jpg",
+    description: "Container Haulage Freight",
+  },
+  {
+    preview: "/images/beach-4206785__340.jpg",
+    original: "/images/beach-4206785_1280.jpg",
+    description: "Aerial Beach View",
+  },
+  {
+    preview: "/images/flowers-1835619__340.jpg",
+    original: "/images/flowers-1835619_1280.jpg",
+    description: "Flower Blooms",
+  },
+  {
+    preview: "/images/mountains-3674334__340.jpg",
+    original: "/images/mountains-3674334_1280.jpg",
+    description: "Alpine Mountains",
+  },
+  {
+    preview: "/images/landscape-4208571__340.jpg",
+    original: "/images/landscape-4208571_1280.jpg",
+    description: "Mountain Lake Sailing",
+  },
+  {
+    preview: "/images/the-alps-4209272__340.jpg",
+    original: "/images/the-alps-4209272_1280.jpg",
+    description: "Alpine Spring Meadows",
+  },
+  {
+    preview: "/images/landscape-4208255__340.jpg",
+    original: "/images/landscape-4208255_1280.jpg",
+    description: "Nature Landscape",
+  },
+  {
+    preview: "/images/lighthouse-4208843__340.jpg",
+    original: "/images/lighthouse-4208843_1280.jpg",
+    description: "Lighthouse Coast Sea",
+  },
+];
